a520457a2733611ed673d6563f73b929f2a8cf8f
Aditi <aditig006.com>
2020-09-29 16:23:17 +0530

    updated services

--- routes/categories.js
+++ routes/categories.js
@@ -4,27 +4,22 @@ const router = express.Router();
 
 const models = require('../lib/database/mysql/index');
 const utils = require('../data/utils');
-const cache = require('../lib/cache/redis');
 const middlwr_caching = require('../data/middlewares/caching_functions');
 const joi_validtn = require('../data/joi');
+const category_services = require('../services/categories');
+
 
 
 // Get all categories
 router.get('/', middlwr_caching.caching_all_catg, async(req, res) => {
 
-    let [err, CATEGORIES] = await to(models.categoryModel.findAll({
-        attributes: {exclude: ['createdAt', 'updatedAt']}
-      }));
-
+    let [err, serv] = await to(category_services.get_all_catg());
     if(err)
         return res.json({ data: null, error: err});
+    let [error, CATEGORIES] = serv;
 
-    let CATG = JSON.stringify( CATEGORIES, null, 0);
-
-    let data;
-    [err, data] = await to(cache.setValue("All_Categories", CATG));
-    if(err)
-        return res.json({ data: null, error: "Eror in setting value in Redis !!"});
+    if(error)
+        return res.json({ data: null, error});
 
     return res.send({ data: CATEGORIES, error: null});
 });
@@ -32,21 +27,14 @@ router.get('/', middlwr_caching.caching_all_catg, async(req, res) => {
 
 // Get category by id
 router.get('/:category_id', async(req, res) => {
-    let cat_id = req.params.category_id;
 
-    let [err, CATEGORY] = await to(models.categoryModel.findOne(
-        {   attributes: {exclude: ['createdAt', 'updatedAt']}  ,
-            where: {
-            id: cat_id
-            }
-        }
-    ));
-
+    let [err, serv] = await to(category_services.get_category_by_id(req.params.category_id));
     if(err)
-        return res.json({data: null, error: err});
-
-    if( CATEGORY == null)
-        return res.json({ data: null, error: "No category found with this id !"});
+        return res.json({ data: null, error: err});
+    
+    let [error,CATEGORY] = serv;
+    if(error)
+        return res.json({ data: null, error});
     
     return res.send({ data: CATEGORY, error: null});
 
@@ -56,21 +44,14 @@ router.get('/:category_id', async(req, res) => {
 // Get the category of a product id
 router.get('/inProduct/:product_id', async(req, res) => {
 
-    let prod_id = req.params.product_id;
-
-    let [err, Product] = await to(models.productModel.findAll(
-        {   
-            where: {id: prod_id},
-            include: [{   model: models.categoryModel }]
-        }
-    ));
-
+    let [err, serv] = await to(category_services.get_category_of_prod_id(req.params.product_id));
     if(err)
         return res.json({ data: null, error: err});
-
-    let {id, name, desc} = Product[0].dataValues.category.dataValues;
-    let category = { id, name, desc}
-
+    
+    let [error, category] = serv;
+    if(error)
+        return res.json({ data: null, error});
+    
     return res.json({ data:category, error: null});
 
 });
@@ -83,20 +64,15 @@ router.post('/', utils.verifyToken, async(req, res) => {
     let cust = res.cur_customer;
 
     if( cust.id != process.env.admin_id)
-    {
         return res.json({ data: null, error: "Only admins can add a category !!"});
-    }
 
     // Validation
     let validated = await joi_validtn.vldt_add_category.validate(category);
 
     if(validated && validated.error)
-    {
         return res.json({ data: null, error: validated["error"].message });
-    }
 
     const newCategory = models.categoryModel.build(category);
-
     await newCategory.save();
 
     return res.json({ data: "Category added successfully !", error: null});
--- routes/shopping_cart.js
+++ routes/shopping_cart.js
@@ -55,76 +55,20 @@ router.post('/add', utils.verifyToken, async(req, res) => {
     
     let cart_product = req.body;
     let cust = res.cur_customer;
-    let prod_id = cart_product.product_id;
 
     // Validation
     let validated = await joi_validtn.vldt_add_to_cart.validate(cart_product);
 
     if(validated && validated.error)
-    {
         return res.json({ data: null, error: validated["error"].message });
-    }
 
-    
-    // Checking the product exist or not
-    let [err, serv] = await to(product_services.get_prod_by_id(prod_id) );
 
+    let [err, serv] = await to(cart_services.add_to_cart(cart_product, cust));
     if(err)
         return res.json({data: null, error: err});
 
-    let [error, PRODUCT] = serv;
-
-    if(error)
-        res.json({ data: null, error})
-
-
-    // Checking if the user has already added th eproduct to cart or not- if yes, add the quantity to that entry
-    let CART_ENTRY;
-    [err, CART_ENTRY ] = await to(models.cartModel.findOne(
-        {   attributes: {exclude: ['createdAt', 'updatedAt']},
-            where: {
-                customer_id: cust.id,
-                product_id: cart_product.product_id
-            }
-        }
-    ));
-
-    if(err)
-        return res.json({data: null, error: err});
-
-
-    if( CART_ENTRY != null)
-    {
-        let new_qty = CART_ENTRY.quantity + cart_product.quantity;
-            let new_subtotal = (new_qty)*(PRODUCT.discounted_price);
-            
-            let [tmp, count] = await to(models.cartModel.update(
-                { 
-                    quantity: new_qty,
-                    subtotal: new_subtotal
-                }, 
-                {
-                    where: {
-                        id: CART_ENTRY.id
-                    }
-                }
-            ));
-    
-        return res.json({ data: "Added to cart !!", error: null});
-    }
-
-
-    let n_product = {
-        'customer_id': cust.id,
-        'product_id': cart_product.product_id,
-        'product_name': PRODUCT.dataValues.name,
-        'quantity': cart_product.quantity,
-        'unit_cost': PRODUCT.dataValues.discounted_price,
-        'subtotal': (cart_product.quantity)*(PRODUCT.discounted_price)
-    }
-
-    const new_product = models.cartModel.build(n_product);
-    await new_product.save();
+    if(serv[0])
+        return res.json({ data: null, error: serv[0] });
 
     return res.json({ data: "Added to cart !", error: null}); 
 });
@@ -139,64 +83,22 @@ router.put('/update/:product_id', utils.verifyToken, async(req, res) => {
     let validated = await joi_validtn.vldt_update_cart.validate( req.body );
 
     if(validated && validated.error)
-    {
         return res.json({ data: null, error: validated["error"].message });
-    }
 
        
     let prod_id = req.params.product_id;
     let qty = req.body.quantity;
-    let cust = res.cur_customer;
 
-    
-    // Checking the product exist or not
-    let [err, serv] = await to(product_services.get_prod_by_id(prod_id) );
 
-    if(err)
-        return res.json({data: null, error: err});
-    
-    let [error, PRODUCT] = serv;
-
-    if(error)
-        res.json({ data: null, error})
-
-    // Checking if the user has already added the product to cart or not- if yes, add the quantity to that entry
-    let CART_ENTRY;
-    [err, CART_ENTRY ] = await to(models.cartModel.findOne(
-        {   attributes: {exclude: ['createdAt', 'updatedAt']},
-            where: {
-                customer_id: cust.id,
-                product_id: prod_id
-            }
-        }
-    ));
-
+    let [err, serv] = await to(cart_services.update_prod_in_cart( prod_id, qty, res.cur_customer));
     if(err)
         return res.json({data: null, error: err});
 
+    if(serv[0])
+        return res.json({ data: null, error: serv[0] });
 
-    if( CART_ENTRY != null)
-    {
-        let new_subtotal = (qty)*(PRODUCT.discounted_price);
-            
-        let [tmp, count] = await to(models.cartModel.update(
-            { 
-                quantity: qty,
-                subtotal: new_subtotal
-            }, 
-                
-            {
-                where: {
-                    id: CART_ENTRY.id
-                }
-            }
-        ));
     
-        return res.json({ data: " Updated product in cart !!", error: null});
-    }
-
-    return res.json({ data: null, error: "This product is not present in your cart !!"});
-
+    return res.json({ data: " Updated product in cart !!", error: null});
 
 });
 
